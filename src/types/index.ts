7a152f82c0e1ef53b0080322ec75bce2642c4036
vansoundz <ndiwa.ek@gmail.com>
2022-04-20 07:37:23 +0300

    include planet type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,25 @@
+type Planet = {
+    name: string
+    rotation_period: string
+    orbital_period: string
+    diameter: string
+    climate: string
+    gravity: string
+    terrain: string
+    surface_water: string
+    population: string
+}
+
 type Person = {
     name: string
     height: string
     mass: string
     gender: string
-    homeworld: string
+    homeworld: Planet
   }
 
   export type {
-      Person
+    Person,
+    Planet
   }
   
